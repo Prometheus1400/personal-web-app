edbfddccf8503aeead563918406d8361e6e2ba86
Prometheus1400 <prometheus1400@gmail.com>
2022-06-10 20:59:35 -0500

    added form submission and link icons

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
